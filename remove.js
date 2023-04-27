68fe8693c7f51b545fd28f775fa8ade42f6bf0a5
tcpcon <89215892+tcpcon@users.noreply.github.com>
2023-04-27 13:35:01 +0100

    Update remove.js

--- remove.js
+++ remove.js
@@ -1,12 +1,8 @@
 if (document.querySelector("#root > div > div.q-box > div:nth-child(2) > div") != undefined) { // if the page contains login prompt
-    const signInBox = document.querySelector("#root > div > div.q-box > div:nth-child(2) > div");
-    signInBox.remove(); // remove sign-in box
+    document.querySelector("#root > div > div.q-box > div:nth-child(2) > div").remove(); // remove sign-in box
 
-    const background = document.querySelector("#root > div > div.q-box > div:nth-child(1)");
-    background.style.filter = "" // remove background blur
-
-    const body = document.querySelector("body");
-    body.style.overflow = "" // remove scroll lock
+    document.querySelector("#root > div > div.q-box > div:nth-child(1)").style.filter = ""; // remove background blur
+    document.querySelector("body").style.overflow = ""; // remove scroll lock
 
     console.log("Removed block!")
 }
