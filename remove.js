0b80b1c815244ee1212dc45433bdf023e7c81d08
tcpcon <89215892+tcpcon@users.noreply.github.com>
2023-04-27 13:11:43 +0100

    Update remove.js

--- remove.js
+++ remove.js
@@ -1,4 +1,4 @@
-if (document.querySelector("#root > div > div.q-box > div:nth-child(2) > div") != undefined) {
+if (document.querySelector("#root > div > div.q-box > div:nth-child(2) > div") != undefined) { // if the page contains login prompt
     const signInBox = document.querySelector("#root > div > div.q-box > div:nth-child(2) > div");
     signInBox.remove(); // remove sign-in box
 
